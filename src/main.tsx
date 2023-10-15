05cfea61ffa09d703f606fa32b79ea5228d83a40
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 22:20:04 +0100

    fix: add CartProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App.tsx';
+import { CartProvider } from './context/CartContext.tsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>
 );
