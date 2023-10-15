18537a9c7bd6de5b8015c1ed310ee4ad883772c9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 17:21:07 +0100

    fix: modify

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
 
 const Navbar = () => {
   return (
-    <NavbarBs className='bg-white shadow-sm mb-3'>
+    <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
       <Container>
         <Nav className='me-auto'>
           <Nav.Link to='/' as={NavLink}>
