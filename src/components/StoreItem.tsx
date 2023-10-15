fc65432d7197a81b5111314c7e28f5c18f5cb0a3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 17:54:50 +0100

    fix: add markup

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -12,6 +12,12 @@ const StoreItem: FC<StoreItemProps> = ({ id, name, price, imgUrl }) => {
   return (
     <Card>
       <Card.Img variant='top' src={imgUrl} alt='' className='card-img' />
+      <Card.Body className='d-flex flxe-column'>
+        <Card.Title className='d-flex justify-content-space-between align-items-baseline mb-4'>
+          <span className='fs-2'>{name}</span>
+          <span className='ms-2 text-muted'>{price}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   );
 };
