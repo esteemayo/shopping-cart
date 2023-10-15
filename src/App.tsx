03678cfccab144a03737bfefb1aa2aa31b64b114
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 16:50:32 +0100

    fix: handling routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import About from './pages/About';
+import Home from './pages/Home';
+import Store from './pages/Store';
+
+import Layout from './components/Layout';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='store' element={<Store />} />
+            <Route path='about' element={<About />} />
+          </Route>
+        </Routes>
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
