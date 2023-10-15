6a88ea5dc9e92492e8cea7201a10f09f494f0af7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 21:52:45 +0100

    fix: add css styling

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -15,7 +15,7 @@ const StoreItem: FC<StoreItemProps> = ({ id, name, price, imgUrl }) => {
     <Card>
       <Card.Img variant='top' src={imgUrl} alt='' className='card-img' />
       <Card.Body className='d-flex flxe-column'>
-        <Card.Title className='d-flex justify-content-space-between align-items-baseline mb-4'>
+        <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
           <span className='fs-2'>{name}</span>
           <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
         </Card.Title>
