1fea8285902b28ea04972058275c6bf4cdb6298b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 22:08:35 +0100

    fix: add markup

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 
 import { formatCurrency } from '../utils/formatCurrency';
 
@@ -11,14 +11,40 @@ interface StoreItemProps {
 }
 
 const StoreItem: FC<StoreItemProps> = ({ id, name, price, imgUrl }) => {
+  const quantity = 0;
+
   return (
-    <Card>
+    <Card className='h-100'>
       <Card.Img variant='top' src={imgUrl} alt='' className='card-img' />
       <Card.Body className='d-flex flex-column'>
         <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
           <span className='fs-2'>{name}</span>
           <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
         </Card.Title>
+        <div className='mt-auto'>
+          {quantity === 0 ? (
+            <Button className='w-100'>+ Add to cart</Button>
+          ) : (
+            <div
+              className='d-flex flex-column align-items-center'
+              style={{ gap: '0.5rem' }}
+            >
+              <div
+                className='d-flex align-items-center justify-content-center'
+                style={{ gap: '0.5rem' }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className='fs-3'>{quantity}</span> in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant='danger' size='sm'>
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
