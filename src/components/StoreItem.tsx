08c6d6c7365b791c7deacb736dce81fea9b8b05b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 17:44:22 +0100

    fix: add static type annotation

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,13 @@
-const StoreItem = () => {
+import { FC } from 'react';
+
+interface StoreItemProps {
+  readonly id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+}
+
+const StoreItem: FC<StoreItemProps> = ({ id, name, price, imgUrl }) => {
   return <div>StoreItem</div>;
 };
 
