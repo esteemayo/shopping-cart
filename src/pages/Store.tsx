0b3be9c4ff613f4ae09251b21b5882d4590753da
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-15 17:40:53 +0100

    fix: add markup

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,23 @@
+import { Col, Row } from 'react-bootstrap';
+
+import storeItems from '../data/item.json';
+import StoreItem from '../components/StoreItem';
+
 const Store = () => {
-  return <div>Store</div>;
+  return (
+    <div>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className='g-3'>
+        {storeItems.map((item) => {
+          return (
+            <Col>
+              <StoreItem key={item.id} {...item} />
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
 };
 
 export default Store;
