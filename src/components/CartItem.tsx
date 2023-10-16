78786c656fd87655cb84acbe21d47f2c58b321dd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-16 03:14:33 +0100

    fix: add markup

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,7 +1,8 @@
 import { FC } from 'react';
-import { Stack } from 'react-bootstrap';
+import { Button, Stack } from 'react-bootstrap';
 
 import storeItems from '../data/item.json';
+import { formatCurrency } from '../utils/formatCurrency';
 import { useCart } from '../context/CartContext';
 
 interface CartItemProps {
@@ -18,8 +19,27 @@ const CartItem: FC<CartItemProps> = ({ id, quantity }) => {
   }
 
   return (
-    <Stack>
+    <Stack direction='horizontal' gap={2} className='d-flex align-items-center'>
       <img src={item.imgUrl} alt='' className='cart-img' />
+      <div className='me-auto'>
+        <div>
+          {item.name}{' '}
+          {quantity > 1 && (
+            <span className='text-muted cart-quantity'>x{quantity}</span>
+          )}
+        </div>
+        <div className='text-muted cart-price'>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div>{formatCurrency(item.price * quantity)}</div>
+      <Button
+        variant='outline-danger'
+        size='sm'
+        onClick={() => removeFromCart(id)}
+      >
+        &times;
+      </Button>
     </Stack>
   );
 };
