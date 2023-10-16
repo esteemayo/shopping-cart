3f69124adb9b4130f0e8c2c7ddbe0fa8d13570ee
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-16 14:35:35 +0100

    fix: calculate cart total price

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,10 +1,28 @@
 import { Offcanvas, Stack } from 'react-bootstrap';
 
-import CartItem from './CartItem';
 import { useCart } from '../context/CartContext';
+import { formatCurrency } from '../utils/formatCurrency';
+
+import CartItem from './CartItem';
+import storeItems from '../data/item.json';
+import { useMemo } from 'react';
 
 const Cart = () => {
   const { isOpen, cart, closeCart } = useCart();
+
+  const totalPrice = useMemo(() => {
+    const { total } = cart.reduce(
+      (cartTotal, cartItem) => {
+        const item = storeItems.find((item) => item.id === cartItem.id);
+        const itemTotal = (item?.price ?? 0) * cartItem.quantity;
+        cartTotal.total += itemTotal;
+        return cartTotal;
+      },
+      { total: 0 }
+    );
+
+    return total;
+  }, [cart]);
 
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement='end'>
@@ -16,6 +34,9 @@ const Cart = () => {
           {cart.map((item) => {
             return <CartItem key={item.id} {...item} />;
           })}
+          <div className='ms-auto fw-bold fs-5'>
+            Total: {formatCurrency(totalPrice)}
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
