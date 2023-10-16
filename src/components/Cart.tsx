24be19cddd17415cf69987e8b26852f09fdacf38
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-10-16 02:40:21 +0100

    feat: add CartItem component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,14 +1,23 @@
-import { Offcanvas } from 'react-bootstrap';
+import { Offcanvas, Stack } from 'react-bootstrap';
+
+import CartItem from './CartItem';
 import { useCart } from '../context/CartContext';
 
 const Cart = () => {
-  const { isOpen, closeCart } = useCart();
+  const { isOpen, cart, closeCart } = useCart();
 
   return (
     <Offcanvas show={isOpen} onHide={closeCart} placement='end'>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cart.map((item) => {
+            return <CartItem key={item.id} {...item} />;
+          })}
+        </Stack>
+      </Offcanvas.Body>
     </Offcanvas>
   );
 };
